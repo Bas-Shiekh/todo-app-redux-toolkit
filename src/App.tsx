2e61170cf6b0163155c5157fefc7f21922f11077
Bas-sheikh <basilelshakhe@gmail.com>
2022-12-05 23:03:32 +0200

    created react router for handle project pages. relates #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
 import { FC } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LandingPage, SignUpPage, LoginPage, TodoListPage } from "./pages";
 import "./App.css";
 
-const App: FC = () => <div className="App">hi</div>;
+const App: FC = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/signup",
+      element: <SignUpPage />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/list",
+      element: <TodoListPage />,
+    },
+    {
+      path: "*",
+      element: <div>hello from not found page</div>,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
 export default App;

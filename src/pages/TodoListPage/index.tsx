eb38cdd5df2eb041a704c93dbba4548abdf13289
Bas-sheikh <basilelshakhe@gmail.com>
2022-12-05 23:14:03 +0200

    created todo list page component. relates #3

---
+++ src/pages/TodoListPage/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Header } from "../../components";
+import "./index.css";
+
+const TodoListPage: FC = () => {
+  return (
+    <>
+      <Header />
+      <div>hello from todo list page</div>
+    </>
+  );
+};
+
+export default TodoListPage;

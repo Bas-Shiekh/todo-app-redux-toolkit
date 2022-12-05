5956099704f9c678a16d7eab9d2239fb76f09fc1
Bas-sheikh <basilelshakhe@gmail.com>
2022-12-05 23:13:02 +0200

    created login page component. relates #3

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Header } from "../../components";
+import "./index.css";
+
+const LoginPage: FC = () => {
+  return (
+    <>
+      <Header />
+      <div>hello from Login page</div>
+    </>
+  );
+};
+
+export default LoginPage;

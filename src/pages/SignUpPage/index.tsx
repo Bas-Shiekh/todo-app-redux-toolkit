b17f1c165f31c0a0586fcf14feae08228e094a29
Bas-sheikh <basilelshakhe@gmail.com>
2022-12-05 23:13:36 +0200

    created signup page component. relates #3

---
+++ src/pages/SignUpPage/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Header } from "../../components";
+import "./index.css";
+
+const SignUpPage: FC = () => {
+  return (
+    <>
+      <Header />
+      <div>hello from sign up page</div>
+    </>
+  );
+};
+
+export default SignUpPage;

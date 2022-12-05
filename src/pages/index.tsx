2baf9825b7fd3b8c930f9b85dfdf210be4941c9d
Bas-sheikh <basilelshakhe@gmail.com>
2022-12-05 23:14:44 +0200

    import pages components and export. relates #3

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import LandingPage from "./LandingPage";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import TodoListPage from "./TodoListPage";
+
+export { LandingPage, LoginPage, SignUpPage, TodoListPage };
